55c4455bcf451d9b01622845ced498e5c71474d7
thscksdnr19 <cksdnr1019@gmail.com>
2022-05-09 19:06:42 +0900

    EX commit

--- FCFS.c
+++ FCFS.c
@@ -2,20 +2,20 @@
 
 int main()
 {
-    int n, i, j, at[20], bt[20], t1, t2, ct[20], tat[20], wt[20], ttat = 0, twt = 0;
+    int num_proc, i, j, at[20], bt[20], t1, t2, ct[20], tat[20], wt[20], ttat = 0, twt = 0;
     double Total_wt = 0, Total_tat = 0;
     printf("Enter the number of process(max 20): ");
-    scanf("%d", &n);
+    scanf("%d", &num_proc);
     printf("\t\t\tEnter the processes\n");
     printf("processes\tArrival Time Burst time\n");
-    for (i = 0; i < n; i++)
+    for (i = 0; i < num_proc; i++)
     {
         printf("process[%d]:\t\t", i + 1);
         scanf("%d%d", &at[i], &bt[i]);
     }
-    for (i = 0; i < n - 1; i++)
+    for (i = 0; i < num_proc - 1; i++)
     {
-        for (j = i + 1; j < n; j++)
+        for (j = i + 1; j < num_proc; j++)
         {
             if (at[i] > at[j])
             {
@@ -29,7 +29,7 @@ int main()
             }
         }
     }
-    for (i = 0; i < n; i++)
+    for (i = 0; i < num_proc; i++)
     {
         if (i == 0)
         {
@@ -47,7 +47,7 @@ int main()
             }
         }
     }
-    for (i = 0; i < n; i++)
+    for (i = 0; i < num_proc; i++)
     {
         tat[i] = ct[i] - at[i];
         wt[i] = ct[i] - at[i] - bt[i];
@@ -55,13 +55,13 @@ int main()
         twt = twt + wt[i];
     }
     printf("processes\tarrival\tburst\tcompletion\twaiting\tturnaround\n");
-    for (i = 0; i < n; i++)
+    for (i = 0; i < num_proc; i++)
     {
         printf("process[%d]\t[%d]\t[%d]\t[%d]\t\t[%d]\t[%d]\n", i + 1, at[i], bt[i], ct[i], wt[i], tat[i]);
         Total_wt += wt[i];
         Total_tat += tat[i];
     }
-    printf("Average waiting time : %.2f\n", Total_wt / n);
-    printf("Average turnaround time : %.2f\n", Total_tat / n);
+    printf("Average waiting time : %.2f\n", Total_wt / num_proc);
+    printf("Average turnaround time : %.2f\n", Total_tat / num_proc);
     return 0;
 }
