0a975ac5ea0a2c2b73f88579d3e6fabd83b8bd83
2017250297-choi <97ckdtn@gmail.com>
2022-05-31 01:01:16 +0900

    copye compare functions to header
    사용이 용이하도록 compare 파일에 있던 qsort용 비교 함수들을 proc_data.h헤더로 복사해왔습니다.

--- proc_data.h
+++ proc_data.h
@@ -11,5 +11,8 @@ typedef struct {
 	int rem;//remain burst time
 	int c;//완료했으면 0 아니면 -1
 }PROC;
-
+//열린 파일로부터 데이터를 읽어 프로세스 배열을 채우는 함수
 int Process_load(FILE* fp,int* num_proc,int* tq, PROC* procs);
+
+int compare_a(const void* p1, const void* p2);//arrival 순서 퀵 정렬을 위한 compare 함수
+int compare_p(const void* p1, const void* p2);//pid순으로 정렬하기위한 compare 함수
