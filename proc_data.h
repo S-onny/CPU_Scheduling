947c10a3b9e2bfbb36391aa04e00d10f397aa5d7
2017250297-choi <97ckdtn@gmail.com>
2022-05-31 20:20:39 +0900

    Update proc_data.h
    DATA구조체에 결과 table출력을 위한 변수들 추가되었습니다.(총대기시간 총 TA시간 총 반응시간)

--- proc_data.h
+++ proc_data.h
@@ -13,11 +13,16 @@ typedef struct {
 }PROC;
 typedef struct{
 	PROC* procs;
-	int num_proc;
+	int num_proc;//number of processes
+	//for gantt chart&readyQueue sim
 	int* g_p;
 	int* g_et;
 	int* g_bt;
 	int gantt_index;
+	//result-total values
+	int ttat;//turn_around time
+	int twt;//total wait time
+	int trt;//total response time
 }DATA;
 //열린 파일로부터 데이터를 읽어 프로세스 배열을 채우는 함수
 int Process_load(FILE* fp,int* num_proc,int* tq, PROC* procs);
@@ -29,6 +34,13 @@ PROC* Copy_processes(PROC* procs);
 DATA* Make_dataIn(PROC* procArr,int num_proc);
 //데이터 구조배열 전체 제거(free)
 void Destroy_data(DATA* datas);
+
+//wait time, turnaround time 등 스케줄링 결과값을 표로 출력하는 함수
+//twt, ttat, trt를 계산하는 기능 포함
+void Print_table(DATA* datum);
+//Gantt chart를 그리는 함수
+void Print_gantt(DATA* datum);
+
 
 int compare_a(const void* p1, const void* p2);//arrival 순서 퀵 정렬을 위한 compare 함수
 int compare_p(const void* p1, const void* p2);//pid순으로 정렬하기위한 compare 함수
