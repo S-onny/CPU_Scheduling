336027e68c242511c637416619e49c8700a45f07
2017250297-choi <97ckdtn@gmail.com>
2022-06-01 20:29:08 +0900

    scheduling alrothms update
    JSF FCFS pre_pri SRTF

--- srtf.c
+++ srtf.c
@@ -23,6 +23,7 @@ void SRTF(DATA* data)
 	int* g_p=(int*)malloc(sizeof(int)*num_proc);//간트차트를 그리기 위한 동적배열들
 	int* g_et=(int*)malloc(sizeof(int)*num_proc);//간트차트를 그리기 위한 동적배열들
 	int* g_bt=(int*)malloc(sizeof(int)*num_proc);//간트차트를 그리기 위한 동적배열들
+	int gantt_capacity=num_proc;
 	int min_rem=INT_MAX;//실행가능한 프로세스 중 가장 짧은 남은시간
 	int min_rem_index=-1;// 실행가능한 프로세스중 가장 remain time이 짧은 프로세스의 index
 
@@ -71,10 +72,12 @@ void SRTF(DATA* data)
 			}
 			else{//첫번째로 실행되는 것이 아닌 프로세스의 경우
 				if (g_p[gantt_index] != procs[min_rem_index].p) {//이전에 실행된 프로세스와 다른 프로세스일 경우: 다음 index에 저장
-					if (sizeof(g_p)/(sizeof(int)) < gantt_index+2)//동적배열들의 할당크기가 부족할 경우 재할당
-						g_p=(int*)realloc(g_p,sizeof(g_p)*2);
-						g_et=(int*)realloc(g_et,sizeof(g_et)*2);
-						g_bt=(int*)realloc(g_bt,sizeof(g_bt)*2);
+					if (gantt_capacity < gantt_index+2){//동적배열들의 할당크기가 부족할 경우 재할당
+						gantt_capacity *=2;
+						g_p=(int*)realloc(g_p,sizeof(int)*gantt_capacity);
+						g_et=(int*)realloc(g_et,sizeof(int)*gantt_capacity);
+						g_bt=(int*)realloc(g_bt,sizeof(int)*gantt_capacity);
+					}
 					gantt_index++;
 					g_p[gantt_index]=procs[min_rem_index].p;
 					g_et[gantt_index]=time;
@@ -110,171 +113,5 @@ void SRTF(DATA* data)
 		(data->g_p)=g_p;
 		(data->g_et)=g_et;
 		(data->g_bt)=g_bt;
-		(data->g_gantt_index)=gantt_index;
-		
-		for(i=0;i<num_proc;i++){//twt ttat 계산
-			twt += procs[i].wt;
-			ttat += procs[i].tat;
-		}
-		
-		//print table
-	    printf("-------------------------------------------------------------------\n");
-        printf(" Processes\tArrival\tBurst\tCompletion\tWaiting\tTurnaround\n");
-        printf("-------------------------------------------------------------------\n");
-        for (i = 0; i < num_proc; i++)
-        {
-            printf(" Process[%d]\t[%d]\t[%d]\t[%d]\t\t[%d]\t[%d]\n", procs[i].p, procs[i].at, procs[i].bt, procs[i].ct, procs[i].wt, procs[i].tat);
-
-        }
-        printf("-------------------------------------------------------------------\n");
-        printf("Average waiting time : %.2f\n", twt / num_proc);
-        printf("Average turnaround time : %.2f\n", ttat / num_proc);
-		
-		
-		// 간트 차트 그리기
-        printf("\n\n");
-        printf("Gantt Chart");
-        printf("\n");
-
-
-        for (i = 1; i <= gantt_index+1; i++) // 윗줄
-        {
-            printf(" ----");
-
-            for (j = 0; j < g_bt[i - 1]; j++)
-            {
-                printf("-");
-            }
-
-            n = i - 1;
-            if (g_et[i] > g_et[n]+g_bt[n])          // 프로세스 사이에 빈 시간이 있을경우에 출력되는 간트차트의 빈 공간의 윗줄 "-" 
-            {
-                printf(" -");
-                for (k = 0; k < g_et[i] - (g_et[n]+g_bt[n]); k++)
-                {
-                    printf("-");    
-                }
-            }
-        }
-
-
-
-
-
-        printf("\n");
-
-        for (i = 1; i <= gantt_index+1; i++) // 가운데 줄
-        {
-
-            printf("| P%d ", g_p[i - 1]);
-
-            for (j = 0; j < g_bt[i - 1]; j++)
-            {
-                printf(" ");
-            }
-
-            n = i - 1;
-            if (g_et[i] > g_et[n]+g_bt[n])          // 프로세스 사이에 빈 시간이 있을경우에 출력되는 간트차트의 빈 공간의 가운데 줄의 공백
-            {
-                printf("| ");
-                for (k = 0; k < (g_et[i] - (g_et[n]+g_bt[n])); k++)
-                {
-                    printf(" ");
-                }
-            }
-        }
-
-
-
-        printf("|");
-        printf("\n");
-
-        for (i = 1; i <= gantt_index+1; i++) // 아랫줄
-        {
-            printf(" ----");
-
-            for (j = 0; j < g_bt[i - 1]; j++)
-            {
-                printf("-");
-            }
-
-            n = i - 1;
-            if (g_et[i] > g_et[n]+g_bt[n])          // 프로세스 사이에 빈 시간이 있을경우에 출력되는 간트차트의 빈 공간의 아랫줄 "-"
-            {
-                printf(" -");
-                for (k = 0; k < (g_et[i] - (g_et[n]+g_bt[n])); k++)
-                {
-                    printf("-");
-                }
-            }
-        }
-
-
-        printf("\n");
-
-        int a, b, c, d, e, f;
-        for (i = 1; i <= gantt_index+1; i++) // 아랫줄 밑 실행시간
-        {
-            printf("%d    ", g_et[i - 1]);
-            for (j = 0; j < g_bt[i - 1]; j++)
-            {
-                printf(" ");
-            }
-
-            a = g_et[i - 1];                                  // 자릿수만큼 공백 빼기
-            b = 0;
-            while (a != 0)
-            {
-                a = a / 10;
-                ++b;
-            }
-
-            for (c = 0; c < b - 1; c++)
-            {
-                printf("\b");
-            }
-
-            n = i - 1;                              // 프로세스 사이에 빈 시간이 있을경우에 출력되는 간트차트의 빈 공간 밑의 실행시간 사이의 공백
-            if (g_et[i] > g_et[n]+g_bt[n])
-            {
-                printf("%d ", g_et[n]+g_bt[n]);
-                for (k = 0; k < (g_et[i] - (g_et[n]+g_bt[n])); k++)
-                {
-                    printf(" ");
-                }
-
-                d = g_et[n]+g_bt[n];                              // 자릿수만큼 공백 빼기
-                e = 0;
-                while (d != 0)
-                {
-                    d = d / 10;
-                    ++e;
-                }
-
-                for (f = 0; f < e - 1; f++)
-                {
-                    printf("\b");
-                }
-
-            }
-
-        }
-
-
-
-        printf("%d", g_et[gantt_index]+g_bt[gantt_index]);
-        printf("\n");
-        // 간트 차트 끝
-		
-		//재실행?
-		E=0;//E초기화
-		while(E!=1 &&E!=2){
-			printf("Re do? : y:1/n:2 \n");
-			scanf("%d",&E);
-			getchar();
-		}
-		if (E==2) break;
-	
-	
-
+		(data->gantt_index)=gantt_index;
 }
