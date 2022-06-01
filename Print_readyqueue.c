1f162fadd51685572ee7c390464524cf7d96e6a6
2017250297-choi <97ckdtn@gmail.com>
2022-06-02 05:29:04 +0900

    Update Print_readyqueue.c

--- Print_readyqueue.c
+++ Print_readyqueue.c
@@ -37,14 +37,15 @@ void Print_readyQueue(DATA* datum){
 		int pat[datum->num_proc];
 		int pct[datum->num_proc];
 		int pp[datum->num_proc];
+		int prem[datum->num_proc];
 		for (i=0;i<=(gi);i++){
 			ct[i]=bt[i]+et[i];
 		}
-		for (i=0;i<=datum->num_proc;i++){
+		for (i=0;i<datum->num_proc;i++){
 			pat[i]=(datum->procs)[i].at;
 			pct[i]=(datum->procs)[i].ct;
 			pp[i]=(datum->procs)[i].p;
-			
+			prem[i]=(datum->procs)[i].bt;
 		}
 
         for (a = 0; a <= ct[gi]; a++)             // 마지막 프로세스가 실행 종료될때까지의 시간 a
@@ -52,17 +53,20 @@ void Print_readyQueue(DATA* datum){
             c = -1, e=-1;
             d = 0;
 			for(j=0;j<=gi;j++){// 시간 a 일때 실행중인프로세스 pid찾기
-					if (et[j] <= a && a < ct[j]){
+					if (et[j] <= a && a <= ct[j]){
 						c=p[j];
 						e=j;
-						break;
 					}
 				}
 			printf("|  %d    ", a);
 			backspace(a);
             for (i = 0; i < (datum->num_proc); i++)                  // 시간 a일때 대기중인 프로세스들을 출력하는 알고리즘
             {
-                if ((a >= pat[i] && a < pct[i])&& pp[i] != c)        // 시간 a 일때 도착+ 종료안됨+대기중이였던 프로세스들의 개수
+				if(pp[i] == c) {
+					prem[i] --;
+					continue;
+				}
+                if (a >= pat[i] && a < pct[i])       // 시간 a 일때 도착+ 종료안됨+대기중이였던 프로세스들의 개수
                 {
                     d++;
 					if (d>1){
@@ -71,17 +75,18 @@ void Print_readyQueue(DATA* datum){
 					printf("|     P%d      ", pp[i]);
                     backspace(p[e]);
 					
-                    printf("|     %d      ", (datum->procs)[i].rem);
-                    backspace((datum->procs)[i].rem);
+                    printf("|     %d      ", prem[i]);
+                    backspace(prem[i]);
                     printf("|\n");
                 }
+				
             }
 
             if (d == 0)                 // 대기중인 프로세스가 없을때
             {
                 printf("|             |            |\n");
-                printf(" ------- ------------- ------------ \n");
 			}
+			printf(" ------- ------------- ------------ \n");
         }
         // Ready queue table 끝
 }
